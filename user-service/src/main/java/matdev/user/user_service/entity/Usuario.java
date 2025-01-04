6a124a236895bd2c16413119cbf42c464511f7c8
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-01-04 19:41:49 -0300

    Agregar entidad Usuario con implementación de UserDetails y atributos básicos

---
+++ user-service/src/main/java/matdev/user/user_service/entity/Usuario.java
@@ -0,0 +1,80 @@
+package matdev.user.user_service.entity;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import matdev.user.user_service.utils.enums.Role;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Data
+public class Usuario implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false , unique = true)
+    @NotNull
+    String username;
+
+    @Column(nullable = false)
+    @NotNull
+    String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       return Collections.singletonList(role.getAuthority());
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+ 
+}

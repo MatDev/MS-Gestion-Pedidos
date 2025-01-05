5102c15f9a65b3c333e469bc646a4281ffb40a64
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-01-05 18:09:56 -0300

    Agregar clase RegisterRequest para manejar solicitudes de registro de usuario

---
+++ user-service/src/main/java/matdev/user/user_service/dto/request/RegisterRequest.java
@@ -0,0 +1,16 @@
+package matdev.user.user_service.dto.request;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import matdev.user.user_service.dto.UsuarioDto;
+import matdev.user.user_service.utils.enums.Role;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class RegisterRequest extends UsuarioDto {
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String email;
+    private Role role;
+}

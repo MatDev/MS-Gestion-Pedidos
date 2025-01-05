70142191e34fb36f3f9a9f49a876f05064163e19
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-01-05 18:10:21 -0300

    Agregar clase GlobalExeptionHandler para manejo global de excepciones

---
+++ user-service/src/main/java/matdev/user/user_service/exeption/handler/GlobalExeptionHandler.java
@@ -0,0 +1,9 @@
+package matdev.user.user_service.exeption.handler;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExeptionHandler {
+
+}

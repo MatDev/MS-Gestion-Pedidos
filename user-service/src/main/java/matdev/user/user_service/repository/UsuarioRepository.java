e11f11c289d34b00c6bbf5a708fcf5ff68462e1a
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-03-19 00:16:41 -0300

    Modificar UsuarioRepository para extender PagingAndSortingRepository y CrudRepository

--- user-service/src/main/java/matdev/user/user_service/repository/UsuarioRepository.java
+++ user-service/src/main/java/matdev/user/user_service/repository/UsuarioRepository.java
@@ -2,13 +2,14 @@ package matdev.user.user_service.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import matdev.user.user_service.entity.Usuario;
 
 
 
-public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
+public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> , CrudRepository<Usuario, Long>{
     Optional<Usuario> findByEmail(String email);
 
 }

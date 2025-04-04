9eaff231396ebff5df40fba0f9c8ae174f2a4863
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-04-04 14:09:27 -0300

    Agregar configuración de seguridad con autenticación y CORS en SecurityBeansConfig

--- user-service/src/main/java/matdev/user/user_service/config/SecurityBeansConfig.java
+++ user-service/src/main/java/matdev/user/user_service/config/SecurityBeansConfig.java
@@ -1,16 +1,72 @@
 package matdev.user.user_service.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import lombok.RequiredArgsConstructor;
+import matdev.user.user_service.repository.UsuarioRepository;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityBeansConfig {
+
+    private final UsuarioRepository usuarioRepository;
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean 
+    public UserDetailsService userDetailsService(){
+        return  username -> usuarioRepository.findByEmail(username)
+        .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService){
+       DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+       authProvider.setUserDetailsService(userDetailsService);
+         authProvider.setPasswordEncoder(passwordEncoder());
+         return authProvider;
+    }
+
+    
+      @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setExposedHeaders(Arrays.asList("*"));
+        config.setMaxAge(3600L); // 1 hora
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+
 
 }

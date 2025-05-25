67ba55adfb38a7ce20b11d70ff5edf6a3ca21757
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-05-25 04:49:09 -0400

    Update refreshToken method to throw IOException instead of Exception

--- user-service/src/main/java/matdev/user/user_service/service/AuthenticationService.java
+++ user-service/src/main/java/matdev/user/user_service/service/AuthenticationService.java
@@ -5,7 +5,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import matdev.user.user_service.dto.request.AuthenticationRequestDto;
 import matdev.user.user_service.dto.response.JwtAuthResponse;
 
+import java.io.IOException;
+
 public interface AuthenticationService {
     JwtAuthResponse authentication(AuthenticationRequestDto request);
-    void refreshToken(HttpServletRequest request, HttpServletResponse response) throws Exception;
+    void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException;
 } 

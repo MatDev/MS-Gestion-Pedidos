b9e6da3b10ee524c49af9e4ab545f4ee57d193d1
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-06-01 18:44:54 -0400

    Add tenant header constant to AuthConstant

--- user-service/src/main/java/matdev/user/user_service/utils/constants/AuthConstant.java
+++ user-service/src/main/java/matdev/user/user_service/utils/constants/AuthConstant.java
@@ -10,6 +10,7 @@ public class AuthConstant {
         public static final String AUTHORIZATION_HEADER = "Authorization";
         public static final String BEARER_PREFIX = "Bearer ";
         public static final String ROLE_CLAIM = "role";
+        public static final String TENANT_HEADER = "X-Tenant-ID";
     
         public static final String CONTENT_TYPE_JSON = "application/json";
     

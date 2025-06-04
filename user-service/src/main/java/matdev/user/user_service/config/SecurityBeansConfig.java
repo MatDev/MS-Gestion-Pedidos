0b2121dcb0aedc36641f700bd08249589cd8c139
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-06-03 22:28:27 -0400

    Refactor authentication manager to use ProviderManager with TenantAuthenticationProvider

--- user-service/src/main/java/matdev/user/user_service/config/SecurityBeansConfig.java
+++ user-service/src/main/java/matdev/user/user_service/config/SecurityBeansConfig.java
@@ -9,10 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -41,10 +38,10 @@ public class SecurityBeansConfig {
         return new TenantAuthenticationProvider(usuarioRepository, passwordEncoder());
     }
 
-    
-      @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+
+    @Bean
+    public AuthenticationManager authenticationManager(TenantAuthenticationProvider provider) {
+        return new ProviderManager(Collections.singletonList(provider));
     }
 
     @Bean

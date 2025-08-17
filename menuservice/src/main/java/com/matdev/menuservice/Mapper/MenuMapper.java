1f7eab5bf895eb2bac04aa9f73e2399b5b724ad0
Matias-UBB <matias.roman1901@alumnos.ubiobio.cl>
2025-08-16 20:33:08 -0400

    Add MenuMapper class for mapping between Menu, MenuItem, and their DTOs

---
+++ menuservice/src/main/java/com/matdev/menuservice/Mapper/MenuMapper.java
@@ -0,0 +1,31 @@
+package com.matdev.menuservice.Mapper;
+
+
+import com.matdev.menuservice.domain.model.Menu;
+import com.matdev.menuservice.domain.model.MenuItem;
+import com.matdev.menuservice.dto.MenuDto;
+import com.matdev.menuservice.dto.MenuItemDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+    public class MenuMapper {
+        private final ModelMapper mapper = new ModelMapper();
+
+        public MenuDto toDto(Menu entity) {
+            return mapper.map(entity, MenuDto.class);
+        }
+
+        public Menu toEntity(MenuDto dto) {
+            return mapper.map(dto, Menu.class);
+        }
+
+        public MenuItemDto toDto(MenuItem entity) {
+            return mapper.map(entity, MenuItemDto.class);
+        }
+
+        public MenuItem toEntity(MenuItemDto dto) {
+            return mapper.map(dto, MenuItem.class);
+        }
+    }
+
